e0e53ef7185d94a32a388deb6f3f893ff992b081
happy <during4277@naver.com>
2026-04-14 10:21:45 +0900

    Initial commit

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,4 @@
+
+import { viteNodeFetch } from "file:///C:/mySource/Vue/nuxt-board-mongodb/node_modules/@nuxt/vite-builder/dist/vite-node.mjs"
+export default () => viteNodeFetch.getManifest()
+    
